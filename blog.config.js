509a7f36290af1143fd5c74499083c328a7b5b7c
Simon <534500549@qq.com>
2024-06-21 17:53:59 +0800

    1

--- blog.config.js
+++ blog.config.js
@@ -30,7 +30,6 @@ const BLOG = {
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '郭蝈蝈-', // 网站关键词 英文逗号隔开
 
 
-  
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
